b7e76b52adde2d52ab0ba63fe10fd2dc18c8cead
DerrickDipondo <derrickdipondo@gmail.com>
2024-12-06 13:29:38 +0300

    objects_lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+const employee = {
+    name: "Dipondo",
+    streetAddress: 56196,
+}
+//takes three arguments but does not mutate the employee
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+
+    // create a copy of the employee object
+    const newObjectUpdate = { ...employee};
+
+    newObjectUpdate[name] = streetAddress;
+
+    return newObjectUpdate;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const newObjectUpdate = { ...employee};
+    delete newObjectUpdate[key];
+
+    return newObjectUpdate;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+
+
+
+}
